ff29348e99cf23f77fd41331727614c31b0262aa
JAMIL AFZAL <jamilafzal255@gmail.com>
2025-04-19 23:15:56 +0500

    search in rotated sorted array leetcode 33

--- binary search/getPivotInArray.js
+++ binary search/getPivotInArray.js
@@ -13,4 +13,4 @@ const getPivot = (array) => {
 
   return start
 }
-// console.log(getPivot([3, 5, 6, 7, 8, 1, 2]))
+// console.log(getPivot([3, 5, 6, 7, 8, 1]))
---
+++ binary search/searchInSortArray.js
@@ -0,0 +1,42 @@
+const getPivot = (array) => {
+  let start = 0
+  let end = array.length - 1
+  let mid = Math.floor((start + end) / 2)
+  while (start < end) {
+    if (array[mid] >= array[0]) {
+      start = mid + 1
+    } else {
+      end = mid
+    }
+    mid = Math.floor((start + end) / 2)
+  }
+  return start
+}
+
+const binarySearch = (array, key, start, end) => {
+  let mid = Math.floor((start + end) / 2)
+  while (start <= end) {
+    if (array[mid] == key) {
+      return mid
+    }
+    if (key > array[mid]) {
+      start = mid + 1
+    } else {
+      end = mid - 1
+    }
+    mid = Math.floor((start + end) / 2)
+  }
+  return -1
+}
+
+const searchInRotatedArray = (array, key) => {
+  let pivot = getPivot(array)
+  
+  if (key >= array[pivot] && key <= array.length - 1) {
+    return binarySearch(array, key, pivot, array.length - 1)
+  } else {
+    return binarySearch(array, key, 0, pivot - 1)
+  }
+}
+
+console.log(searchInRotatedArray([7, 9, 10, 11, 2, 3, 4], 12))
