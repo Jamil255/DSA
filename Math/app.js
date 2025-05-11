5b332204e882672f849e948cf7abe7e00117f103
Jamil-codeswifters <jamil.afzal@codeswifters.com>
2025-05-11 18:35:04 +0500

    count prime number

--- Math/app.js
+++ Math/app.js
@@ -13,4 +13,22 @@ const isPrime = (num) => {
   }
   return count
 }
-console.log(isPrime(500000))
+// console.log(isPrime(500000))
+
+const countPrime = (n) => {
+    
+    let count = 0
+    let prime = new Array(n).fill(true)
+    prime[0] = prime[1] = false
+    for (let i = 2; i < n; i++) {
+        if (prime[i]) {
+        count++
+        for (let j = 2 * i; j < n; j += i) {
+            prime[j] = false
+        }
+        }
+    }
+    return count
+}
+
+// console.log(countPrime(1000000));
