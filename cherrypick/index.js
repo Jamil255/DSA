b07e5d5372875e592cc7774565cb91cb7e8fa5a4
Jamil255 <jamilafzal255@gmail.com>
2025-11-20 12:04:50 +0500

    pre-commit-hooks

--- cherrypick/index.js
+++ cherrypick/index.js
@@ -3,3 +3,5 @@ console.log('this is a cherrypick index file')
 console.log("this is second commit on github")
 
 console.log("currently learning git hooks")
+
+console.log("this is pre-commit hooks")
